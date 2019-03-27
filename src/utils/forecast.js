9ba68385b54d6c8074f3b28458d44bcc0835d211
joseceron <joseluisceron13@gmail.com>
2019-03-27 00:28:16 -0500

    Add high to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,16 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/eae24c6ec598f25e5df6f7a4951129a4/'+ latitude + ',' + longitude + '?units=si&lang=es'
-
+    const url = 'https://api.darksky.net/forecast/eae24c6ec598f25e5df6f7a4951129a4/'+ latitude + ',' + longitude 
     request({ url, json: true }, (error, { body })=>{
         if (error){
             callback('Unable to connect to location services!', undefined)
         }else if (body.error){
             console.log('Unable to find location', undefined)
         }else {
+            console.log(body.daily.data[0])
             callback('', {
-                temperature: body.daily.data[0].summary + ' It is currently: ' + body.currently.temperature + '  degress out'
+                temperature: body.daily.data[0].summary + ' It is currently: ' + body.currently.temperature + '  degress out. This high today is ' + body.daily.data[0].temperatureHigh 
                 
             })
         }
